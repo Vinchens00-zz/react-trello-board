beaec554bc96c4f841e347664ce1553963f054a4
vinchens00 <it.vinogradov@yandex.ru>
2018-01-29 14:55:51 +0600

    Small fix

--- like-trello-board-react/src/utils/notFound.js
+++ like-trello-board-react/src/utils/notFound.js
@@ -2,7 +2,7 @@ import COMMON from '../enums/common';
 
 export default function (err) {
   if (err.status === 404) {
-    location.replace(COMMON.NOT_FOUND_URL);
+    location.assign(COMMON.NOT_FOUND_URL);
   } else {
     throw err;
   }
